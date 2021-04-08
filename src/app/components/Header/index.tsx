ff629d850a136f2186fc7b3e2e738db25c1ddd74
Vedran Brnjetic <Vedran.Brnjetic@metropolia.fi>
2021-04-08 20:05:15 +0300

    Header component styled

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -9,7 +9,31 @@ import styled from 'styled-components/macro';
 interface Props {}
 
 export function Header(props: Props) {
-  return <Div></Div>;
+  return (
+    <Div>
+      <Logo />
+      <Title>Nord Software</Title>
+    </Div>
+  );
 }
 
-const Div = styled.div``;
+const Div = styled.div`
+  padding: 32px;
+  width: 912px;
+  background-color: #adb5bd;
+  width: 976px;
+`;
+const Logo = styled.img.attrs(props => ({
+  src: 'https://fakeimg.pl/32/',
+}))`
+  display: inline;
+  vertical-align: middle;
+`;
+const Title = styled.h1`
+  display: inline;
+  margin-left: 16px;
+  vertical-align: middle;
+  font-size: 24px;
+  color: #ffffff;
+  font-weight: 500;
+`;
--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
+import { Header } from 'app/components/Header';
 
 export function HomePage() {
   return (
     <>
       <Helmet>
-        <title>Home Page</title>
-        <meta name="description" content="A Boilerplate application homepage" />
+        <title>Participants | Digia</title>
+        <meta name="description" content="Participants page for Digia" />
       </Helmet>
+      <Header></Header>
       <span>HomePage container</span>
     </>
   );
