108f4a19feb953722f3a8150d38e7fc1541f32ac
Vedran Brnjetic <Vedran.Brnjetic@metropolia.fi>
2021-04-08 19:18:51 +0300

    Created Header component

---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,15 @@
+/**
+ *
+ * Header
+ *
+ */
+import * as React from 'react';
+import styled from 'styled-components/macro';
+
+interface Props {}
+
+export function Header(props: Props) {
+  return <Div></Div>;
+}
+
+const Div = styled.div``;
